42317c9e6ae8f0b48ec10b31468be30f4f223d9e
Adelina <adelina.abdykanyeva@gmail.com>
2022-10-27 19:09:58 -0500

    Spring Data Queries

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,14 @@
+package com.cydeo.entity;
+
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+
+    @Id
+    private Integer id;
+
+}
