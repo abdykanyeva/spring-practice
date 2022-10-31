5c372a27fcdc8922857adb3e8e2b2c531cf5cea3
Adelina <adelina.abdykanyeva@gmail.com>
2022-10-31 12:04:07 -0500

    Spring Data Queries

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -3,9 +3,11 @@ package com.cydeo.repository;
 import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
@@ -14,12 +16,14 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
     //Write a derived query to get cinema with a specific name
 
-    List<Cinema> findByName(String name);
+   Optional<Cinema> findByName(String name);
 
 
     //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
 
     List<Cinema> findDistinctTop3BySponsoredName(String name);
+
+    List<Cinema> findFirst3BySponsoredNameContainingOrderBySponsoredName(String sponsoredName);
 
     //Write a derived query to list all cinemas in a specific country
 
@@ -34,8 +38,8 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
     //Write a JPQL query to read the cinema name with a specific id
 
-    @Query("SELECT e FROM Cinema e WHERE e.id = ?1")
-    List<Cinema> readWithSpecificId(Long id);
+    @Query("SELECT e.name FROM Cinema e WHERE e.id = ?1")
+    String readWithSpecificId(@Param("id")Long id);
 
 
 
@@ -43,23 +47,29 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
     //Write a native query to read all cinemas by location country
 
-    @Query(value = "SELECT * FROM cinema WHERE location_id = ?1", nativeQuery = true)
-    List<Cinema> readAllCinemasByLocation(String location);
+    @Query(value = "SELECT * FROM cinema c JOIN location l" +
+            "ON l.id = c.location_id WHERE l.country = ?1", nativeQuery = true)
+    List<Cinema> readAllCinemasByLocation(@Param("locationCountry")String locationCountry);
 
 
     //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
+
+    @Query(value="SELECT * FROM cinema WHERE name ILIKE concat('%', ?1, '%')" +
+            "OR  sponsored_name ILIKE concat('%', ?1, '%')", nativeQuery = true)
+    List<Cinema> readByNameSponsoredName(@Param("pattern")String pattern);
 
 
     //Write a native query to sort all cinemas by name
 
     @Query(value="SELECT * FROM cinema ORDER BY name", nativeQuery = true)
-    List<Cinema> sortAllByName(String name);
+    List<Cinema> sortAllByName();
 
 
     //Write a native query to distinct all cinemas by sponsored name
 
 
     @Query(value="SELECT DISTINCT sponsored_name FROM cinema", nativeQuery = true)
-    List<Cinema> distinctAllCinemas(String name);
+    List<String> distinctAllCinemas(String name);
+
 
 }
