34db69c7f3b2629fefe400e373a2c84cabb60d91
Adelina <adelina.abdykanyeva@gmail.com>
2022-11-08 15:26:28 -0600

    Spring Data

---
+++ spring-11-Data-TableCreationPractice/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO EMPLOYEES(id, name)
+value(1, 'Mike');
+
+INSERT INTO students( first_name, last_name, email)
+VALUES( 'Mike', 'Smith' , 'mike@gmail.com',)
---
+++ spring-11-Data-TableCreationPractice/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE EMPLOYEES(
+
+    id bigint not null,
+    name varchar(255),
+    primary key (id)
+
+);
