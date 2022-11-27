ab0d9d965bcb360aace0e7667555a4defcf82bc8
Adelina <adelina.abdykanyeva@gmail.com>
2022-11-27 09:46:15 -0600

    Spring Rest

--- spring-14-Data-QueriesPractice/src/main/java/com/cydeo/repository/DepartmentRepository.java
+++ spring-14-Data-QueriesPractice/src/main/java/com/cydeo/repository/DepartmentRepository.java
@@ -1,6 +1,7 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Department;
+import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,6 +11,13 @@ public interface DepartmentRepository extends JpaRepository<Department, String> 
     // Display all departments in the furniture department
 
     List<Department> findByDepartment(String department);
+
+
+    // Display all departments in the Health Division
+
+    List<Department> findByDivision(String division);
+
+    // Display all employees that firstname is not ""
 
 
 
