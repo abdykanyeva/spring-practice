1657f48dc7076c570c2fb5030407b573fce14413
Adelina <adelina.abdykanyeva@gmail.com>
2022-12-07 13:33:18 -0600

    Spring-create API

--- spring-15-REST-practice/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
+++ spring-15-REST-practice/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
@@ -7,6 +7,7 @@ import com.cydeo.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,12 @@ public class CourseController_ResponseWrapper {
         return ResponseEntity.status(HttpStatus.ACCEPTED)
                 .header("Version", "Cydeo V3")
                 .body(new ResponseWrapper("Courses retrieved successfully", courseService.getCourses()));
+    }
+
+
+    @GetMapping("{id}")
+    public ResponseEntity<ResponseWrapper> getCourseById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(new ResponseWrapper("Course retrieved by id", courseService.getCourseById(id)));
     }
 
 
