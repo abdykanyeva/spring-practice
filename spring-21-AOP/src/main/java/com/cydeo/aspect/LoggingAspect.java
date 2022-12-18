5b3b1dfe655743e97fec4feb2ff17c8819888f92
Adelina <adelina.abdykanyeva@gmail.com>
2022-12-18 15:39:26 -0600

    Spring AOP

---
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -0,0 +1,77 @@
+package com.cydeo.aspect;
+
+
+import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+//    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
+//    public void myPointcut(){}
+//
+//    @Before("myPointcut()")
+//    public void log(){
+//        System.out.println("Info log .......");
+//    }
+
+//    @Before("execution(* com.cydeo.controller.CourseController.*(..))")
+////    public void log(){
+////        System.out.println("Info log .......");
+////    }
+
+//    @Pointcut("execution(* com.cydeo.repository.CourseRepository.findById(*))")
+//    public void courseRepositoryFindByIdPC(){}
+//
+//
+//    @Before("courseRepositoryFindByIdPC()")
+//    public void beforeCourseRepositoryFindById(JoinPoint joinPoint){
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {} "
+//        , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//    }
+
+//    @Pointcut("within(com.cydeo.controller..*)")
+//    public void anyControllerOperation(){}
+//
+//    @Pointcut("@within(org.springframework.stereotype.Service)")
+//    public void anyServiceOperation(){}
+//
+//    @Before("anyControllerOperation() || anyServiceOperation()")
+//    public void beforeControllerOrServiceAdvice(JoinPoint joinPoint){
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {} "
+//        , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//
+//    }
+
+//    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+//    public void anyDeleteControllerOperation(){}
+//
+//
+//    @Before("anyDeleteControllerOperation()")
+//    public void beforeDeleteMappingAnnotation(JoinPoint joinPoint){
+//
+//        logger.info("Before -> Method: {}, Arguments: {}, Target: {} "
+//        , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//
+//
+//    }
+
+    @Pointcut("@annotation(com.cydeo.annotation.LoggingAnnotation)")
+    public void loggingAnnotationPC(){}
+
+    @Before("loggingAnnotationPC()")
+    public void beforeLoggingAnnotation(JoinPoint joinPoint){
+        logger.info("Before -> Method: {}, Arguments: {}, Target: {} "
+        , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+
+    }
+}
